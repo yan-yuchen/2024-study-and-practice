d42a3ab7e17cedeaed7216743696361277ac526a
chenchen <2436503722@qq.com>
2024-05-14 18:49:26 +0800

    2024 5 14

--- bus_data/main.py
+++ bus_data/main.py
@@ -13,24 +13,20 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-def station(html, keywords, bus_station, buslines_information, buslines_polyline, city, station_frequency):
+
+def station(html, keywords, bus_station, buslines_information, buslines_polyline, city):
     buslines = html["buslines"]
     count = 0
     for i in buslines:
-        count += 1
+        count = count + 1
         busstops = i["busstops"]
-
         """站点信息"""
         for T in busstops:
-            station_id = T["id"]
-            if station_id in station_frequency:
-                station_frequency[station_id] += 1
-            else:
-                station_frequency[station_id] = 1
-
-            lng1, lat1 = T["location"].split(",")
-            cell = [city, T["name"], WGS1984.main(lng1, lat1)[0], WGS1984.main(lng1, lat1)[1], station_id, i["name"],
-                    count, station_frequency[station_id]]
+            cell = []
+            lng1 = T["location"].split(",")[0]
+            lat1 = T["location"].split(",")[1]
+            cell = [city, T["name"], WGS1984.main(lng1, lat1)[0], WGS1984.main(lng1, lat1)[1], T["id"], i["name"],
+                    count]
             with open(bus_station, 'a', newline='', encoding='utf-8') as f:
                 write = csv.writer(f)
                 write.writerow(cell)
@@ -48,7 +44,7 @@ def station(html, keywords, bus_station, buslines_information, buslines_polyline
         cou = 0
         buslines_name2 = i["name"]
         for i in split_polyline:
-            cou += 1
+            cou = cou + 1
             excessive = i.split(",")
             bus_polyline = [city, cou, buslines_name2, keywords[0:4], WGS1984.main(excessive[0], excessive[1])[0],
                             WGS1984.main(excessive[0], excessive[1])[1]]
@@ -56,64 +52,80 @@ def station(html, keywords, bus_station, buslines_information, buslines_polyline
                 write = csv.writer(f)
                 write.writerow(bus_polyline)
 
-def research(city, keywords, key, bus_station, buslines_information, buslines_polyline, station_frequency):
+
+def research(city, keywords, key, bus_station, buslines_information, buslines_polyline):
     url = "https://restapi.amap.com/v3/bus/linename?parameters"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36"}
-    keyword = {"s": "rsv3", "extensions": "all", "key": key, 'jscode': jscode, "output": "json", "city": city, "offset": "2",
+    keyword = {"s": "rsv3", "extensions": "all", "key": key, 'jscode':jscode , "output": "json", "city": city, "offset": "2",
                "keywords": keywords, "platform": "JS"}
     a = requests.get(url, headers=headers, params=keyword)
     html = a.text
     data = json.loads(html)
     if data["status"] == "1":
-        station(data, keywords, bus_station, buslines_information, buslines_polyline, city, station_frequency)
+        station(data, keywords, bus_station, buslines_information, buslines_polyline, city)
     else:
         print("未查询到{}市的{}信息".format(city, keywords))
 
+
 def main(city, key, province):
-    if not os.path.isdir(os.getcwd() + "/data"):
+    if os.path.isdir(os.getcwd() + "/data") == False:
         os.mkdir(os.getcwd() + "/data")
-    if not os.path.isdir(os.getcwd() + "/data/" + province):
+    if os.path.isdir(os.getcwd() + "/data/" + province) == False:
         os.mkdir(os.getcwd() + "/data/" + province)
+
     now_path = os.getcwd() + "/data/" + province + "/csv/"
-    if not os.path.isdir(now_path):
+    if os.path.isdir(now_path) == False:
         os.mkdir(now_path)
 
     bus_station = now_path + city + "公交站点.csv"
     buslines_information = now_path + city + "公交线路信息.csv"
     buslines_polyline = now_path + city + "公交线路.csv"
-    station_frequency = {}
 
     with open(bus_station, 'a', newline='', encoding='utf_8_sig') as f:
         write = csv.writer(f)
-        write.writerow(["城市", "站点名称", "lng", "lat", "id", "线路名称", "正反线路", "线路频次"])
+        write.writerow(["城市", "站点名称", "lng", "lat", "id", "name", "正反线路"])
 
     with open(buslines_information, 'a', newline='', encoding='utf_8_sig') as f:
         write = csv.writer(f)
-        write.writerow(["城市", "正反线路", "简称", "全称", "上车票价", "全程票价", "距离", "起点站", "终点站", "线路类型", "首班时间", "末班时间"])
+        write.writerow(
+            ["城市", "正反线路", "简称", "全称", "上车票价", "全程票价", "距离", "起点站", "终点站", "线路类型",
+             "首班时间", "末班时间"])
 
     with open(buslines_polyline, 'a', newline='', encoding='utf_8_sig') as f:
         write = csv.writer(f)
         write.writerow(["城市", "序号", "全称", "简称", "lng", "lat"])
 
     citybus_name = citybus.main(city)
+    """
+    df = pd.read_excel("/Users/creative/Documents/python/Bus route data/dtt.xlsx")
+    citys = df[df["字段1"] == city]
+    citys = citys.reset_index()
+    citybus_name = []
+    for i in range(len(citys)):
+        citybus_name.append({"city":city,"name":citys.loc[i,"字段2"]})
+    """
 
     print("【正在获取{}公交线路数据】\n".format(city))
 
     for i in tqdm(citybus_name):
         keywords = i["name"]
-        research(city, keywords, key, bus_station, buslines_information, buslines_polyline, station_frequency)
+        city = i["city"]
+        research(city, keywords, key, bus_station, buslines_information, buslines_polyline)
+        time.sleep(random.randint(0, 1))
 
-
-    df = pd.read_csv(bus_station)
+    # 新建字段存储线路频次
+    df = pd.read_csv('./data/黑龙江/csv/哈尔滨公交站点.csv')
+    # 根据站点名称分组，并对name字段进行合并，使用', '作为分隔符
+    # 同时创建一个新字段'线路频次'，初始化为1
+    df['线路频次'] = 1
     df = df.groupby('站点名称').agg({
-    'lng': 'first',  # 选择每个组的第一个lng值
-    'lat': 'first',  # 选择每个组的第一个lat值
-    '正反线路': 'first',  # 选择每个组的第一个正反线路值
-    '线路名称': lambda x: ', '.join(x),  # 将线路名称用逗号连接起来
-    '线路频次': 'sum'  # 对线路频次进行求和
-    })
-    df.reset_index(inplace=True)
+        'lng': 'first',
+        'lat': 'first',
+        '正反线路': 'first',
+        'name': lambda x: ', '.join(x),
+        '线路频次': 'sum'
+    }).reset_index()
     df.to_csv(now_path + city + "公交站点.csv", index=False, encoding='utf_8_sig')
 
     conversion_geo.main(bus_station, buslines_information, buslines_polyline, city, province)
